082e968668e5eaec0289ae650a3f1f71fb61755d
Oleg Aculov <oaculov@gmail.com>
2024-07-06 23:16:10 -0400

    port

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require('express')
+const express = require('express');
 const routes = require("./routes");
 const bParser = require("./bodyParser");
 
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 
 bParser(app);
